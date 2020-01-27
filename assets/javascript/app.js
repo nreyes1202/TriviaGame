ac22f099460eb4ba456573474b381ec721747e7c
Nilsen Reyes <nreyes1202@yahoo.com>
2020-01-26 23:25:18 -0700

    added ecomap and div elements

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Timer will count down
+    // Add timer
+    // setInterval
+    //on 0, add Times' Up! alert
+// User will click on a choice for each question
+    // Add click events (or selector of some sort?)
+        //user will choose a, b, or c
+            //One of these is correct
+                // Player can't pick more than one answer
+// Time runs out
+    // Display number of correct and incorrect questions
